bc6b71b907237add2a667f5f61c120bd5cc5771d
HARRY AMOAFO FYNN <109153581+streetprophet90@users.noreply.github.com>
2024-07-26 18:38:14 +0000

    Created views and URLs

--- ahajahcreo/bidding/views.py
+++ ahajahcreo/bidding/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Auction, Bid
+
+def index(request):
+    auctions = Auction.objects.all()
+    return render(request, 'bidding/index.html', {'auctions': auctions})
+
+def auction_detail(request, auction_id):
+    auction = get_object_or_404(Auction, pk=auction_id)
+    return render(request, 'bidding/auction_detail.html', {'auction': auction})
+
