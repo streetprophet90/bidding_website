6351164c94e900551ac2262c6a3b9ccb3af2fb68
HARRY AMOAFO FYNN <109153581+streetprophet90@users.noreply.github.com>
2024-07-26 19:22:48 +0000

    urls updated views and logins created

--- ahajahcreo/bidding/urls.py
+++ ahajahcreo/bidding/urls.py
@@ -1,4 +1,3 @@
-# bidding/urls.py
 
 from django.urls import path
 from . import views
@@ -6,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('auction/<int:auction_id>/', views.auction_detail, name='auction_detail'),
+    path('auction/new/', views.create_auction, name='create_auction'),
 ]
+
--- ahajahcreo/bidding/views.py
+++ ahajahcreo/bidding/views.py
@@ -1,6 +1,8 @@
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Auction
+from .forms import AuctionForm
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     auctions = Auction.objects.all()
@@ -9,6 +11,19 @@ def index(request):
 def auction_detail(request, auction_id):
     auction = get_object_or_404(Auction, pk=auction_id)
     return render(request, 'bidding/auction_detail.html', {'auction': auction})
+
+@login_required
+def create_auction(request):
+    if request.method == 'POST':
+        form = AuctionForm(request.POST)
+        if form.is_valid():
+            auction = form.save(commit=False)
+            auction.creator = request.user
+            auction.save()
+            return redirect('index')
+    else:
+        form = AuctionForm()
+    return render(request, 'bidding/create_auction.html', {'form': form})
 
 def home(request):
     return render(request, 'bidding/home.html')
